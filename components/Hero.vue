ceda6c4439cf83cd5148154fe5b6a2b3a498731a
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-02-16 20:03:48 +0100

    Initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="py-12 px-4 sm:px-6 lg:px-8">
+    <div class="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
+      <div class="flex items-center justify-center space-x-3">
+        <img loading="lazy" src="/logo.png" alt="Slice of Byte" class="h-20 w-20" />
+        <h1
+          class="text-4xl font-extrabold tracking-tight text-slate-700 sm:text-5xl"
+        >
+          Slice of Byte
+        </h1>
+      </div>
+      <p class="mt-4 text-base leading-7 text-slate-600">
+        <a
+          class="text-primary underline"
+          href="https://sliceofbyte.io"
+          target="_blank"
+          >sliceofbyte.io</a
+        >
+        is a blog that breaks down the Go language specification into byte-sized, 
+        digestible chunks. It’s designed to help you learn Go effortlessly, 
+        one small and easy-to-understand piece at a time.
+      </p>
+      <p class="mt-4 text-base leading-7 text-slate-600">
+        Gopher artwork by Ashley McNamara licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 License.
+      </p>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.form-input {
+  @apply block h-10 w-full rounded-md bg-white pr-3 pl-12 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-900/5 placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6;
+}
+.form-submit {
+  @apply inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 ml-4 flex-none;
+}
+</style>
---
+++ seoConfig/index.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Slice of Byte",
+  description:
+    "sliceofbyte.io is a blog that breaks down the Go language specification into byte-sized, digestible chunks.",
+  og: {
+    title: "Slice of Byte",
+    description:
+      "sliceofbyte.io is a blog that breaks down the Go language specification into byte-sized, digestible chunks.",
+    image: "https://sliceofbyte.io/logo.png",
+    url: "https://sliceofbyte.io",
+  },
+};
